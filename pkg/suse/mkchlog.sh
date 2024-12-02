88cde9e8546c2407196a0dba4f8f7b33299becbe
Pablo Suárez Hernández <psuarezhernandez@suse.com>
2024-12-02 15:54:43 +0000

    Add multiple changelog files and mkchlog script

---
+++ pkg/suse/mkchlog.sh
@@ -0,0 +1,17 @@
+echo "Generating changelog entry for Salt package"
+if ! osc vc _temp.changes;
+then
+    exit 1;
+fi
+
+echo "Update changelog files"
+echo >> _temp.changes
+echo "$(cat _temp.changes salt.changes)" > salt.changes
+git add salt.changes
+
+for i in $(ls changelogs/*changes); do
+    echo "$(cat _temp.changes $i)" > $i
+    git add $i
+done
+
+rm _temp.changes

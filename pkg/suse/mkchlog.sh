c5c9bcaedf4f2d9bf942318b8223d85a219df092
Pablo Suárez Hernández <psuarezhernandez@suse.com>
2024-12-10 13:59:45 +0000

    Fix mkchlog script

--- pkg/suse/mkchlog.sh
+++ pkg/suse/mkchlog.sh
@@ -6,8 +6,6 @@ fi
 
 echo "Update changelog files"
 echo >> _temp.changes
-echo "$(cat _temp.changes salt.changes)" > salt.changes
-git add salt.changes
 
 for i in $(ls changelogs/*/salt.changes); do
     echo "$(cat _temp.changes $i)" > $i
